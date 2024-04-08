c4c9d9b974fbdc50a75f8308e5a1ee0756464e9c
jcbarros24 <126578116+jcbarros24@users.noreply.github.com>
2024-04-08 20:33:02 -0300

    Commit calculadora

---
+++ demo/src/main/java/com/example/demo/MathController.java
@@ -0,0 +1,50 @@
+package com.example.demo;
+
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@RestController
+public class MathController {
+
+    private final AtomicLong counter = new AtomicLong();
+    
+
+    @RequestMapping(value = "/sum/{numberOne}/{numberTwo}", 
+            method=RequestMethod.GET)
+    public Double sum(
+            @PathVariable(value = "numberOne") String numberOne, 
+            @PathVariable(value = "numberTwo") String numberTwo
+        ) throws Exception{
+
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+            throw new Exception();
+        }
+        return convertToDoulbe(numberOne) + convertToDoulbe(numberTwo);
+    }
+    
+
+    private Double convertToDoulbe(String number) {
+        if (number == null)
+            return 0D;
+        // BR 21,24 US 21.24
+        String numbeString = number.replaceAll(",",".");
+        if (isNumeric(numbeString)) 
+            return Double.parseDouble(numbeString);
+        return 0D;
+    }
+
+
+    private boolean isNumeric(String number){
+        if (number == null)
+            return false;
+            String numbeString = number.replaceAll(",",".");
+        return numbeString.matches("[-+]?[0-9]*\\.?[0-9]+");
+    }
+}
