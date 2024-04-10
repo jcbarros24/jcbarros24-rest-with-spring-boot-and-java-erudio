9317a53dd0ab972a451fe5bde0b75962693121f7
jcbarros24 <126578116+jcbarros24@users.noreply.github.com>
2024-04-10 20:57:35 -0300

    Commit

--- demo/src/main/java/com/example/demo/MathController.java
+++ demo/src/main/java/com/example/demo/MathController.java
@@ -1,21 +1,17 @@
 package com.example.demo;
 
-
 //import java.util.concurrent.atomic.AtomicLong;
-
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.example.demo.exceptions.UnsupportedMathOperationException;
-
+import com.mathOperations.MathLogicOperations;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 
 @RestController
 public class MathController {
 
-    //private final AtomicLong counter = new AtomicLong();
+    //private final AtomicLong counter = new AtomicLong();      
     
     @RequestMapping(value = "/sum/{numberOne}/{numberTwo}", 
             method=RequestMethod.GET)
@@ -24,28 +20,56 @@ public class MathController {
             @PathVariable(value = "numberTwo") String numberTwo
         ) throws Exception{
 
-        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
-            throw new UnsupportedMathOperationException("Please set a numeric value!");
-        }
-        return convertToDouble(numberOne) + convertToDouble(numberTwo);
-    }
-    
-
-    private Double convertToDouble(String number) {
-        if (number == null)
-            return 0D;
-        // BR 21,24 US 21.24
-        String numbeString = number.replaceAll(",",".");
-        if (isNumeric(numbeString)) 
-            return Double.parseDouble(numbeString);
-        return 0D;
+        return MathLogicOperations.sum(numberOne, numberTwo);
     }
 
+    @RequestMapping(value = "/sub/{numberOne}/{numberTwo}", 
+            method=RequestMethod.GET)
+    public Double subtraction(
+            @PathVariable(value = "numberOne") String numberOne, 
+            @PathVariable(value = "numberTwo") String numberTwo
+        ) throws Exception{
 
-    private boolean isNumeric(String number){
-        if (number == null)
-            return false;
-            String numbeString = number.replaceAll(",",".");
-        return numbeString.matches("[-+]?[0-9]*\\.?[0-9]+");
+        return MathLogicOperations.sub(numberOne, numberTwo);
     }
+
+    @RequestMapping(value = "/multiplication/{numberOne}/{numberTwo}", 
+            method=RequestMethod.GET)
+    public Double multiplication(
+            @PathVariable(value = "numberOne") String numberOne, 
+            @PathVariable(value = "numberTwo") String numberTwo
+        ) throws Exception{
+
+        return MathLogicOperations.multiplication(numberOne, numberTwo);
+    }
+
+    @RequestMapping(value = "/division/{numberOne}/{numberTwo}", 
+            method=RequestMethod.GET)
+    public Double division(
+            @PathVariable(value = "numberOne") String numberOne, 
+            @PathVariable(value = "numberTwo") String numberTwo
+        ) throws Exception{
+
+        return MathLogicOperations.division(numberOne, numberTwo);
+    }
+
+    @RequestMapping(value = "/medium/{numberOne}/{numberTwo}", 
+            method=RequestMethod.GET)
+    public Double medium(
+            @PathVariable(value = "numberOne") String numberOne, 
+            @PathVariable(value = "numberTwo") String numberTwo
+        ) throws Exception{
+
+        return MathLogicOperations.medium(numberOne, numberTwo);
+    }
+
+    @RequestMapping(value = "/squareRoot/{numberOne}", 
+            method=RequestMethod.GET)
+    public Double square(
+            @PathVariable(value = "numberOne") String numberOne
+        ) throws Exception{
+
+        return MathLogicOperations.squareRoot(numberOne);
+    } 
+
 }
